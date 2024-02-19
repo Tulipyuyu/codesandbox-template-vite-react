ed6ca92353aa70445c05113a18aab209c318c975
yu wang <wtulipyu@gmail.com>
2024-02-19 06:58:26 +0000

    测试pdf 文件

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import { useState } from "react";
 import "./App.css";
+import { jsPDF } from "jspdf";
 
 function App() {
   const [count, setCount] = useState(0);
-
+  const handleSavePdf = () => {
+    const doc = new jsPDF();
+    doc.text("Hello world!", 10, 10);
+    doc.save("a4.pdf");
+  };
   return (
     <div className="App">
       <div>TEST</div>
       <button onClick={() => setCount((count) => count + 1)}>
         count is {count}
       </button>
+      <button onClick={handleSavePdf}>PDF TEST</button>
     </div>
   );
 }
